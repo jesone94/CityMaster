4cb1d4c1a3224628b935e2d5efa589bd2e436330
Dmitrii Penkin <jesone94@gmail.com>
2021-06-08 14:57:30 +0400

    reitings

--- city-master/src/components/StatsElement/StatsEl.jsx
+++ city-master/src/components/StatsElement/StatsEl.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import style from './StatsEl.css';
 export default function StatsEl({ data, index }) {
   console.log(data, 'DATA');
   return (
-    <div>
-      <img src={data.urlImg}></img>
-      <span>{index + 1}</span>
+    <div className={'stats-el'}>
+      <span>№{index + 1}</span>
+      <div>
+        <img src={data.urlImg} width={'50px'} height={'50px'}></img>
+      </div>
+
       <h1>
         {data.displayName} набрал {data.score}
       </h1>
