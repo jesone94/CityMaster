fb429b7f4d7194d57bbbadd55c8e99d90a3de757
Dmitrii Penkin <jesone94@gmail.com>
2021-06-11 15:44:54 +0400

    Fix

--- back/app.js
+++ back/app.js
@@ -3,15 +3,17 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 
-app.use(morgan("dev"))
+app.use(morgan('dev'));
 
-const buildHTML = path.resolve(__dirname, "../city-master/buildindex.html");
-const buildState = path.resolve(__dirname, "../city-master/build")
+const buildHTML = path.resolve(__dirname, '../city-master/build/index.html');
+const buildState = path.resolve(__dirname, '../city-master/build/');
+
+app.use(express.static(buildState));
 
 app.get('*', (_, res) => {
-  res.sendFile(buildHTML)
-})
+  res.sendFile(buildHTML);
+});
 
 app.listen(8080, () => {
-  console.log('Server started at port 8080')
-})
+  console.log('Server started at port 8080');
+});
