4731aba309990cdeae5d9dd090a360301c5dd766
Dmitrii Penkin <jesone94@gmail.com>
2021-06-11 16:50:38 +0400

    Fixxxxxxx

--- back/app.js
+++ back/app.js
@@ -14,6 +14,6 @@ app.get('*', (_, res) => {
   res.sendFile(buildHTML);
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('Server started at port 8080');
 });
